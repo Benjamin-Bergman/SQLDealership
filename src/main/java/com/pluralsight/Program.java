ed528d0ecb8c7577a78a94fafda5f1d3297c0bfb
Benjamin-Bergman <git@gdane.net>
2024-06-14 10:23:11 -0400

    Remove unnecessary comments

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -12,16 +12,7 @@ import java.io.*;
 
 @SuppressWarnings("UtilityClass")
 final class Program {
-//    private static final File FILE_PATH = new File("inventory.csv");
-//    private static final File CONTRACT_PATH = new File("contracts.csv");
-
     public static void main(String[] args) {
-//        var dealership = new ResourceBackedDealership(
-//            new BasicDealership("Default_Name", "Default_Address", "Default_Phone"),
-//            () -> new FileReader(FILE_PATH),
-//            append -> new FileWriter(FILE_PATH, append));
-//        var contracts = new FileBackedContractList(CONTRACT_PATH);
-
         var dealership = new SQLBackedDealership(1);
         var contracts = dealership.getContractManager();
 
